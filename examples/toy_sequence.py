a67a6deec86fbd348533a9156b5ec6bc563aa4fc
Hannes <schulz@ais.uni-bonn.de>
2015-07-26 03:17:54 +0200

    toy example for sequence learning works quite well

--- examples/toy_sequence.py
+++ examples/toy_sequence.py
@@ -9,7 +9,7 @@ import theano.tensor as T
 from passage.theano_utils import floatX
 from passage.models import RNN
 from passage.iterators import Padded
-from passage.updates import NAG, Regularizer
+from passage.updates import NAG, Regularizer, Adam
 from passage.layers import Generic, GatedRecurrent, Dense, SimpleRecurrent
 from passage.utils import load, save
 
@@ -17,21 +17,27 @@ from passage.utils import load, save
 batch_size = 64
 data_dim = 10
 n_classes = 4
-max_sent_len=10
+max_sent_len=15
 y_lag = 4
 trX, teX, trY, teY = toy_sequence_dataset.generate((1000, 500), data_dim=data_dim,
                                                    n_phrase_labels=n_classes,
                                                    max_sent_len=max_sent_len,
-                                                   tag_end=True)
+                                                   min_sent_len=max_sent_len-3,
+                                                   n_phrase_words=4,
+                                                   n_phrases=20,
+                                                   n_words=15,
+                                                   tag_end=False, label_noise=0.0)
 
 layers = [
 	Generic(size=data_dim),
-	SimpleRecurrent(size=32, p_drop=0.2, seq_output=True),
+	GatedRecurrent(size=32, p_drop=0.1, seq_output=True),
+	GatedRecurrent(size=32, p_drop=0.1, seq_output=True),
 	Dense(size=n_classes, activation='softmax', p_drop=0.5)
 ]
 
 #A bit of l2 helps with generalization, higher momentum helps convergence
-updater = NAG(momentum=0.95, regularizer=Regularizer(l2=1e-4))
+# updater = NAG(momentum=0.95, regularizer=Regularizer(l2=1e-4))
+updater = Adam(lr=0.006)
 padder = Padded(size=batch_size, x_dtype=floatX, y_lag=y_lag)
 
 #Linear iterator for real valued data, cce cost for softmax
@@ -41,8 +47,15 @@ model.fit(trX, trY, n_epochs=100, batch_size=batch_size)
 tr_preds = model.predict(trX[:len(teY)])
 te_preds = model.predict(teX)
 
-tr_acc = np.mean([np.mean(y == np.argmax(yhat[y_lag:],axis=1)) for y, yhat in zip(trY[:len(teY)], tr_preds)])
-te_acc = np.mean([np.mean(y == np.argmax(yhat[y_lag:],axis=1)) for y, yhat in zip(teY, te_preds)])
+tr_acc = np.mean([np.all(y == np.argmax(yhat[y_lag:],axis=1)) for y, yhat in zip(trY[:len(teY)], tr_preds)])
+te_acc = np.mean([np.all(y == np.argmax(yhat[y_lag:],axis=1)) for y, yhat in zip(teY, te_preds)])
+
+
+print "Examples from Test: "
+S = (["\n".join(map(str, (y, np.argmax(yhat[y_lag:],axis=1))))
+      for y, yhat in zip(teY, te_preds)])
+for s in S[:15]:
+    print s, "\n"
 
 # Test accuracy should be between 98.9% and 99.3%
 print 'train accuracy', tr_acc, 'test accuracy', te_acc
--- examples/toy_sequence_dataset.py
+++ examples/toy_sequence_dataset.py
@@ -32,9 +32,10 @@ def generate(size, data_dim=5, n_phrase_labels=4, n_words=3,
     global dictionary, phrases
 
     # generate dictionary
-    dictionary = uniform(size=(n_words, data_dim))
+    dictionary = uniform(-1.0, 1.0, size=(n_words, data_dim))
 
     # generate n_phrases unique word sequences of length n_phrase_words
+    print "Generating %d phrases" % n_phrases
     phrases = []
     phrase_labels = []
     while len(phrases) != n_phrases:
@@ -44,6 +45,7 @@ def generate(size, data_dim=5, n_phrase_labels=4, n_words=3,
         phrase_labels = 1+randint(n_phrase_labels-1, size=n_phrases)
 
     # generate 'sentences'
+    print "Generating %d sentences" % sum(size)
     Xind = []
     Y = []
     for i in xrange(sum(size)):
@@ -51,11 +53,13 @@ def generate(size, data_dim=5, n_phrase_labels=4, n_words=3,
             sent_len = randint(min_sent_len, max_sent_len+1)
             sent = "".join(map(chr, randint(n_words, size=sent_len)))
             if contains_any_phrase(sent, phrases):
+                print ".",
                 break
         Y.append(np.zeros(sent_len,dtype=np.int))
         Xind.append(sent)
 
     # generate labels for dataset
+    print "Generating labels for the sentences..."
     for phrase, plabel in zip(phrases, phrase_labels):
         for idx, sent in enumerate(Xind):
             start = 0
@@ -69,6 +73,7 @@ def generate(size, data_dim=5, n_phrase_labels=4, n_words=3,
                     Y[idx][sidx] = plabel
                 start += 1
 
+    print "Trafo..."
     # transform dataset to code
     if data_dim > 1:
         X = [[dictionary[ord(c)] for c in sent] for sent in Xind]
@@ -80,8 +85,9 @@ def generate(size, data_dim=5, n_phrase_labels=4, n_words=3,
 
     # training label noise
     for sent in Ytrain:
-        mask = uniform(0., label_noise, size=sent.size) > 0.
+        mask = uniform(size=sent.size) < label_noise
         sent[mask] = randint(n_phrase_labels, size=mask.sum())
+    print "Done."
 
     return Xtrain, Xtest, Ytrain, Ytest
 
